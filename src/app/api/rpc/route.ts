0abefa589530c2cef45ab5c60fcdd6df34f84ad1
Othalos <othalos@verizon.net>
2025-09-10 18:39:41 -0500

    Update route.ts
    s
    
    Signed-off-by: Othalos <othalos@verizon.net>

--- src/app/api/rpc/route.ts
+++ src/app/api/rpc/route.ts
@@ -41,7 +41,7 @@ export async function POST(request: NextRequest) {
   }
 }
 
-export async function OPTIONS(request: NextRequest) {
+export async function OPTIONS() {
   return new NextResponse(null, {
     status: 200,
     headers: {
